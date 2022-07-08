81b9c3f5fc8afc183d13d2f2f38086cfa776918b
harshit1523 <96949845+harshit1523@users.noreply.github.com>
2022-07-08 23:02:35 +0530

    Add files via upload

---
+++ mergeSortedArray.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void merge(int arr1[],int n,int arr2[],int m,int arr3[]){
+    int i = 0, k = 0, j = 0;
+    while(i<n && j<m){
+        if(arr1[i]<arr2[j]){
+            arr3[k++] = arr1[i++];
+        }
+        else{
+            arr3[k++] = arr2[j++];
+        }
+    }
+    while(i<n){
+        int arr3[k++] = int arr1[i++];
+    }
+    while(j<m){
+        int arr3[k++] = int arr1[j++];
+    }
+}
+void print(int ans[],int n){
+    int i = 0;
+    for (int i = 0; i < n;i++){
+        cout << ans[i] << " ";
+
+    }
+    cout << endl;
+}
+int main(){
+    int arr1[5] = {1, 3, 5, 7, 9};
+    int arr2[3] = {2, 4, 6};
+    
+    int arr3[8] = {0};
+    merge(arr1, 5, arr2, 3, arr3);
+    print(arr3, 8);
+    return 0;
+}
